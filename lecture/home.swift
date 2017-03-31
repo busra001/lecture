6c0bdaba6740ba5f5655d8ed862ebaf9e8ddf54e
mini4 <mini4@mini4s-Mac-mini.local>
2017-03-31 10:23:00 +0200

    initial commit

---
+++ lecture/home.swift
@@ -0,0 +1,18 @@
+//
+//  class.swift
+//  lecture
+//
+//  Created by mini4 on 31.03.17..
+//  Copyright © 2017. mini4. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class home:UIViewController{
+    @IBOutlet weak var Playlabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.Playlabel.text = "busra"
+    }
+}
