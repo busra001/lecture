69f23fd0ba712ec955767336e25599824605ef31
mini4 <mini4@mini4s-mac-mini.local>
2017-03-31 09:34:53 +0200

    Initial Commit

---
+++ lecture/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lecture
+//
+//  Created by mini4 on 31.03.17..
+//  Copyright © 2017. mini4. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
